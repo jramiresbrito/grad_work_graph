317c7067fd7725a59c5b71feb8c325cf7b1b86c3
Joao Victor Ramires <joaoramires.engsoft@gmail.com>
2021-05-05 22:19:49 -0300

    Add Product Saving Service Add Products/Games CRUD

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,70 @@
+module Api::V1
+  class ProductsController < ApiController
+    before_action :set_product, only: %i[show update destroy]
+
+    def index
+      @loading_service = ModelLoadingService.new(Product.all, searchable_params)
+      @loading_service.call
+    end
+
+    def create
+      run_service
+    rescue ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
+    end
+
+    def show; end
+
+    def update
+      run_service
+    rescue ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
+    end
+
+    def destroy
+      @product.productable.destroy!
+      @product.destroy!
+    rescue ActiveRecord::RecordNotDestroyed
+      render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
+    end
+
+    private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def searchable_params
+      params.permit({ search: {} }, { order: {} }, :page, :length)
+    end
+
+    def run_service
+      @saving_service = ProductSavingService.new(product_params.to_h, @product)
+      @saving_service.call
+      @product = @saving_service.product
+      render :show
+    end
+
+    def product_params
+      return {} unless params.key?(:product)
+
+      permitted_params = params.require(:product).permit(:id, :name, :description,
+                                                         :image, :price, :productable,
+                                                         :status, :featured,
+                                                         category_ids: [])
+      permitted_params.merge(productable_params)
+    end
+
+    def productable_params
+      productable_type = params[:product][:productable] || @product&.productable_type&.underscore
+      return unless productable_type.present?
+
+      productable_attributes = send("#{productable_type}_params")
+      { productable_attributes: productable_attributes }
+    end
+
+    def game_params
+      params.require(:product).permit(:mode, :release_date, :developer, :system_requirement_id)
+    end
+  end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,15 @@
+class Product < ApplicationRecord
+  include LikeSearchable
+  include Paginatable
+
+  belongs_to :productable, polymorphic: true
+  has_many :categories, through: :product_categories
+  has_one_attached :image
+
+  validates :name, :description, :price, :image, :status, presence: true
+  validates :featured, presence: true, if: -> { featured.nil? }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :price, numericality: { greater_than: 0 }
+
+  enum status: { available: 1, unavailable: 2 }
+end
